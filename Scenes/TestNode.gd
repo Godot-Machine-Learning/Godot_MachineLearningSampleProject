5404fc1ae1aff4ac3c411de815677ce8a4e1d493
JUNZ1 <gigabayt@gmail.com>
2020-08-11 18:48:14 +0200

    Naive Bayes and Log. Regres. are implemented

--- Scenes/TestNode.gd
+++ Scenes/TestNode.gd
@@ -9,24 +9,32 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 var simpleLR = SimpleLinearRegression.new()
 var multiLR = MultipleLinearRegression.new()
+var naiveBC = NaiveBayesClassifier.new()
+var logRegression = LogisticRegression.new()
+
 func _ready():
 	$SLR_Start_Train.connect("pressed",self,"SLRStartTrainingPressed")
 	$SLR_CalculateOutput.connect("pressed",self,"SLRCalculateOutputPressed")
 	simpleLR.connect("TrainingFinished",self,"SLRTrainingFinishedSlot")
+
 	$MLR_Start_Train.connect("pressed",self,"MLRStartTrainingPressed")
 	$MLR_Calculate_Output.connect("pressed",self,"MLRCalculateOutputPressed")
 	multiLR.connect("TrainingFinished",self,"MLRTrainingFinishedSlot")
-	
+
+	$NaiveBayesClassifier_Start_Train.connect("pressed",self,"NBCStartTrainingPressed")
+	$NaiveBayesClassifier_CalculateOutput.connect("pressed",self,"NBCCalculateOutputPressed")
+	naiveBC.connect("TrainingFinished",self,"NBCTrainingFinishedSlot")
+
+	$Logistic_Regression_Start_Train.connect("pressed",self,"LRStartTrainingButtonPressed")
+	$Logistic_Regression_CalculateOutput.connect("pressed",self,"LRCalcualteOutputButtonPressed")
+	logRegression.connect("TrainingFinished",self,"LRTrainingFinishedSlot")
 
 	print(simpleLR.TestFunc())
-	
-	
-	print(multiLR.TestFunc())
-	
-	var naiveBC = NaiveBayesClassifier.new()
-	print(naiveBC.TestFunc())
 
-	var logRegression = LogisticRegression.new()
+	print(multiLR.TestFunc())
+
+	print(naiveBC.TestFunc())
+	
 	print(logRegression.TestFunc())
 
 
@@ -57,10 +65,33 @@ func MLRCalculateOutputPressed():
 func MLRTrainingFinishedSlot(input):
 	print("Training Finished: ",input)
 
+func NBCStartTrainingPressed():
+	var Inputmatrix = [[6,180,12],[6,180,11],[6,170,12],[6,165,10],[5,100,6],[6,150,8],[5,130,7],[5,150,9]]
+	var outputMatrix = [0,0,0,0,1,1,1,1]
+	for eachone in Inputmatrix:
+		naiveBC.InsertMultipleInput(eachone)
+	naiveBC.SetOutputs(outputMatrix)
+	naiveBC.StartTraining()
+
+func NBCCalculateOutputPressed():
+	var testInputMatrix = [6,130,8] 
+	print(naiveBC.CalculateForMultipleInput(testInputMatrix))
+
+func NBCTrainingFinishedSlot(input):
+	print("Training Finished: ",input)
 
 
+func LRStartTrainingButtonPressed():
+	var Inputmatrix = [[6,180,12],[6,180,11],[6,170,12],[6,165,10],[5,100,6],[6,150,8],[5,130,7],[5,150,9]]
+	var outputMatrix = [0,0,0,0,1,1,1,1]
+	for eachone in Inputmatrix:
+		logRegression.InsertMultipleInput(eachone)
+	logRegression.SetOutputs(outputMatrix)
+	logRegression.StartTraining()
 
+func LRCalcualteOutputButtonPressed():
+	var testInputMatrix = [6,130,8] 
+	print(logRegression.CalculateForMultipleInput(testInputMatrix))
 
-
-
-
+func LRTrainingFinishedSlot(input):
+	print("Training Finished: ",input)
